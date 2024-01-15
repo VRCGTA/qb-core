f4661dd957dfcaab884499f726604f648b95b689
yukad2 <myuratq@gmail.com>
2024-01-16 01:12:11 +0900

    set mechanic job payment

--- shared/jobs.lua
+++ shared/jobs.lua
@@ -80,11 +80,11 @@ QBShared.Jobs = {
 		defaultDuty = true,
 		offDutyPay = false,
 		grades = {
-			['0'] = { name = 'Recruit', payment = 50 },
-			['1'] = { name = 'Novice', payment = 75 },
-			['2'] = { name = 'Experienced', payment = 100 },
-			['3'] = { name = 'Advanced', payment = 125 },
-			['4'] = { name = 'Manager', isboss = true, payment = 150 },
+			['0'] = { name = 'Recruit', payment = 200 },
+			['1'] = { name = 'Novice', payment = 500 },
+			['2'] = { name = 'Experienced', payment = 800 },
+			['3'] = { name = 'Advanced', payment = 1000 },
+			['4'] = { name = 'Manager', isboss = true, payment = 1500 },
 		},
 	},
 	mechanic2 = {
