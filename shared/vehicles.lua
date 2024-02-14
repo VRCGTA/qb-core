9da0e1b518a75383652e7cc8d4c8ca4a139028ca
yukad2 <myuratq@gmail.com>
2024-02-15 00:39:32 +0900

    eheli

--- shared/vehicles.lua
+++ shared/vehicles.lua
@@ -728,6 +728,8 @@ local Vehicles = {
     { model = 'formula2',      name = 'R88',                           brand = 'Ocelot',          price = 100000,  category = 'openwheel',      type = 'automobile', shop = 'none', job = 'none' },
     { model = 'formula',       name = 'PR4',                           brand = 'Progen',          price = 100000,  category = 'openwheel',      type = 'automobile', shop = 'none', job = 'none' },
 
+    { model = 'eheli',        name = 'Police Maverick VRC',               brand = 'Buckingham',      price = 110000,  category = 'emergency',      type = 'automobile', shop = 'none', job = 'none' },
+
 }
 
 for i = 1, #Vehicles do
